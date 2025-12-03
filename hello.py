9d5ec05259cd904d4bf66954dd1bdc689a76a813
Mo Afiq <afiq980@gmail.com>
2025-12-04 06:37:17 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,10 @@
+import os
+
 print('hello')
 print('hello')
+
+
+directory = input("Enter the directory to list: ")
+# Vulnerable: An attacker can enter something like "; cat /etc/passwd" to run arbitrary commands
+command = f"ls {directory}"
+os.system(command)
